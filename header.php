ff777cb9c61e2632284a999fddbaed772dedadcf
ritudhuwajyaadav-create <ritudhuwajyaadav@gmail.com>
2026-04-22 21:00:03 +0530

    Add files via upload
    My portfolio website

---
+++ header.php
@@ -0,0 +1,11 @@
+<header class = "navbar">
+<nav class = "menu">
+<ul>
+<li><a href="index.php">Home</a></li>
+<li><a href="#left-box">About</a></li>
+<li><a href="#service">Services</a></li>
+<li><a href="#partner">Partners</a></li>
+<li><a href="#contact">Contact</a></li>
+</ul>
+</nav>
+</header>
